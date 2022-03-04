6045f89a0572c94b90d30b14b479da830a499a88
orenambielli <41933270+orenambielli@users.noreply.github.com>
2022-03-04 18:54:49 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -25,12 +25,11 @@ function idleEverything() {
     element.classList.toggle("no-scroll");
   }
   if ( document.getElementById("mike").classList.contains('mike-walk') )
-document.getElementById("mike").classList.toggle('mike-prone');
-  document.getElementById("joel").classList.contains('joel-walk')
-document.getElementById("joel").classList.toggle('joel-prone'); document.getElementById("coleton").classList.contains('coleton-walk')
-document.getElementById("coleton").classList.toggle('coleton-prone');
-document.getElementById("cart").classList.contains('cart')
-document.getElementById("cart").classList.toggle('no-cart');
+      document.getElementById("mike").classList.toggle('mike-prone');
+      document.getElementById("joel").classList.contains('joel-walk')
+      document.getElementById("joel").classList.toggle('joel-prone');
+      document.getElementById("coleton").classList.contains('coleton-walk')
+      document.getElementById("coleton").classList.toggle('coleton-prone');
 }
 
 function toggleColetonProne() {
